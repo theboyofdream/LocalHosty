1a94ffcef7eb3043770a5377ea0edd7d3108d73d
Abhishek <43162530+theboyofdream@users.noreply.github.com>
2023-12-20 14:27:19 +0530

    feat!: added react-native-svg support & also refactored code

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  View
+} from 'react-native';
+
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+
+        <View>
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+export default App;
